fa884f6a5cdb67fb947f099051d5f123e873c5e7
gczybarna <gczy.barna@gmail.com>
2026-01-02 13:55:40 +0100

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\CountyController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +12,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// County routes
+Route::resource('counties', CountyController::class);
+
+// City routes
+Route::resource('cities', CityController::class);
+
+// City filtering and export routes
+Route::post('cities/filter', [CityController::class, 'filter'])->name('cities.filter');
+Route::get('cities/export/csv', [CityController::class, 'exportCsv'])->name('cities.export.csv');
+Route::get('cities/export/pdf', [CityController::class, 'exportPdf'])->name('cities.export.pdf');
